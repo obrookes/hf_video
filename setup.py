c5daa57df52337a0312b62112cb6d6ed80ecac15
obrookes <dl18206@bristol.ac.uk>
2024-01-30 18:34:17 +0000

    full rebuild..

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="hf_video",
-    version="0.0.1",
+    version="0.0.2",
     description="A package for handling videos in Huggingface",
     package_dir={"": "app"},
     packages=find_packages(where="app"),
